7eddec1154450ea29ba8e55418213763b44592d8
pjonaseq21 <gracjan.chamulati2019@gmail.com>
2023-05-09 18:58:40 +0200

    update working githubicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './assets/pc.png';
 import './App.css';
 import animation from './animate'
+import github from './assets/github.png'
 function App() {
  animation()
 
@@ -9,6 +10,7 @@ function App() {
        <>
        <div id='div' className=" main bg-[url('./assets/pc.png')]  h-screen bg-cover bg-center">
        <span id='text' className='text absolute top-1/4 left-1/3 transform -translate-x-1/4 -translate-y-1/2 text-[4rem] text-[#22c55e]'>React</span>
+      <img id='github' className='w-[120px] absolute top-1/2 left-1/2 transform -translate-x-1/4 -translate-y-1/2 text-white opacity-0' src={github}></img>
        </div>
        </>
      
--- src/animate.js
+++ src/animate.js
@@ -1,53 +1,65 @@
 function animation(){
 
-  document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
 
-    const animation = document.querySelector('#animation');
-    const text = document.querySelector('#text');
+        const animation = document.querySelector('#animation');
+        const text = document.querySelector('#text');
+        const logo = document.querySelector("#github");
     
-    const phrases = ['GitHub',"Linux","Python","Tailwind CSS"];
-    let currentPhrase = 0;
-    let currentLetter = 0;
-    let isDeleting = false;
-    let request;
-    
-    const startDelay = 1000;
-    const phraseDelay = 2000;
-    const typingSpeed = 50;
-    const deletingSpeed = 30;
-    
-    function animate() {
-      const currentText = text.textContent;
-      const currentPhraseText = phrases[currentPhrase].slice(0, currentLetter);
-    
-      if (currentText !== currentPhraseText) {
-        text.textContent = currentPhraseText;
-        request = requestAnimationFrame(animate);
-      } else if (!isDeleting && currentLetter === phrases[currentPhrase].length) {
-        isDeleting = true;
-        setTimeout(() => request = requestAnimationFrame(animate), phraseDelay);
-      } else if (isDeleting && currentLetter === 0) {
-        isDeleting = false;
-        currentPhrase = (currentPhrase + 1) % phrases.length;
-        setTimeout(() => {
-          currentLetter = 0;
-          request = requestAnimationFrame(animate);
-        }, phraseDelay);
-      } else {
-        if (!isDeleting) {
-          currentLetter++;
-        } else {
-          currentLetter--;
+        const phrases = ['GitHub',"Linux","Python","Tailwind"];
+        let currentPhrase = 0;
+        let currentLetter = 0;
+        let isDeleting = false;
+        let request;
+        
+        const startDelay = 1000;
+        const phraseDelay = 2000;
+        const typingSpeed = 50;
+        const deletingSpeed = 100;
+        
+        function animate() {
+          const currentText = text.textContent;
+          const currentPhraseText = phrases[currentPhrase].slice(0, currentLetter);
+        
+          if (currentText !== currentPhraseText) {
+            text.textContent = currentPhraseText;
+            checkGithub();  // wywołanie funkcji checkGithub() podczas aktualizacji tekstu
+            request = requestAnimationFrame(animate);
+          } else if (!isDeleting && currentLetter === phrases[currentPhrase].length) {
+            isDeleting = true;
+            setTimeout(() => request = requestAnimationFrame(animate), phraseDelay);
+          } else if (isDeleting && currentLetter === 0) {
+            isDeleting = false;
+            currentPhrase = (currentPhrase + 1) % phrases.length;
+            setTimeout(() => {
+              currentLetter = 0;
+              request = requestAnimationFrame(animate);
+            }, phraseDelay);
+          } else {
+            if (!isDeleting) {
+              currentLetter++;
+            } else {
+              currentLetter--;
+            }
+            setTimeout(() => {
+              request = requestAnimationFrame(animate);
+            }, isDeleting ? deletingSpeed : typingSpeed);
+          }
         }
-        setTimeout(() => request = requestAnimationFrame(animate), isDeleting ? deletingSpeed : typingSpeed);
-      }
-    }
+        
+        function checkGithub(){
+          if(text.innerHTML == "GitHub" && !isDeleting){
+            logo.style.opacity = '1';
+            logo.classList.remove('hide');
+          }else{
+            logo.style.opacity = '0';
+            logo.classList.add('hide');
+          }
+        }
+        
+        setTimeout(() => request = requestAnimationFrame(animate), startDelay);
+    });
     
-    setTimeout(() => request = requestAnimationFrame(animate), startDelay);
     
-    
-    
-  
-});
 }
 module.exports = animation;
